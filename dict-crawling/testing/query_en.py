0b69ce7cb94d1127b43e1747080966b0d3d7687b
Feathered Hwang <feathered.hwang@hotmail.com>
2020-06-18 20:34:32 +0800

    Managed to connect

--- dict-crawling/testing/query_en.py
+++ dict-crawling/testing/query_en.py
@@ -6,6 +6,40 @@ import scrapy
 from scrapy.crawler import CrawlerProcess
 from scrapy.loader import ItemLoader
 
+from scrapy.loader.processors import Join, MapCompose, TakeFirst
+from w3lib.html import remove_tags
+
+
+class MongoPipeline:
+    collection_name = 'en_scrapy_items'
+
+    def __init__(self, mongo_uri, mongo_db):
+        self.mongo_uri = mongo_uri
+        self.mongo_db = mongo_db
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        # pull in information from settings.py
+        return cls(
+            mongo_uri=crawler.settings.get('MONGO_URI'),
+            mongo_db=crawler.settings.get('MONGO_DATABASE')
+        )
+
+    def open_spider(self, spider):
+        # initializing spider
+        # opening db connection
+        self.client = pymongo.MongoClient(self.mongo_uri)
+        self.db = self.client[self.mongo_db]
+
+    def close_spider(self, spider):
+        # clean up when spider is closed
+        self.client.close()
+
+    def process_item(self, item, spider):
+        # how to handle each post
+        self.db[self.collection_name].insert(dict(item))
+        return item
+
 
 class EnExprItem(scrapy.Item):
     # query
@@ -24,11 +58,15 @@ class EnExprItem(scrapy.Item):
     # files = scrapy.Field()
 
 
+class EnExprLoader(ItemLoader):
+    default_input_processor = TakeFirst()
 
 
 class CamDictSpider(scrapy.Spider):
     name = 'cambridge_dict_spider'
     allowed_domains = ['dictionary.cambridge.org']
+    # expressions
+    exprs = []
 
     custom_settings = {
         # To get the user-agent required for cambridge.org,
@@ -38,25 +76,30 @@ class CamDictSpider(scrapy.Spider):
                       'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
         'ITEM_PIPELINES': {
             'scrapy.pipelines.files.FilesPipeline': 1,
-            # 'MongoPipeline': 100,
+            'dict-crawling.testing.query_en.MongoPipeline': 100,
         },
         # 'FILES_STORE': str(PurePath(__file__).parent.joinpath('CamDictPipeline')),
-
+        'MONGO_URI': 'mongodb://localhost:27017',
+        'MONGO_DATABASE': 'sivji-sandbox',
     }
 
     def __init__(self, exprs=None, *args, **kwargs):
         super(CamDictSpider, self).__init__(*args, **kwargs)
-        exprs = ['recoil', 'blow OFF', ' spELL']
-        # self.start_urls = [ for q in expr]
-        for q in exprs:
-            scrapy.Request(
+        if exprs is None:
+            exprs = []
+        self.exprs = exprs
+        # TODO this line is used for test, delete it later
+        self.exprs = ['recoil', 'blow OFF', ' spELL', 'selected']
+        print(self.exprs)
+
+    def start_requests(self):
+        for q in self.exprs:
+            print(q)
+            yield scrapy.Request(
                 'https://dictionary.cambridge.org/dictionary/english/%s' % q,
                 callback=self.parse,
                 meta={'q': q}
             )
-
-    # def start_requests(self):
-    #     yield scrapy.Request('https://dictionary.cambridge.org/dictionary/english/%s' % self.word)
 
     def parse(self, response):
         # get uk pronunciation url
@@ -64,10 +107,11 @@ class CamDictSpider(scrapy.Spider):
         # url = response.urljoin(src)
         # return EnWordItem(file_urls=[url])
         # return response.css('.uk .daud').get()
+        print(response.url)
 
-        # print(response.request.url)
 
-        il = ItemLoader(item=EnExprItem(), response=response)
+
+        il = EnExprLoader(item=EnExprItem(), response=response)
         # m = re.match(r'(?P<base>.*[/])(?P<expr>[a-z-]+)([?]q=(?P<query>[a-zA-Z0-9%]*))?', response.request.url)
         # q = m.group('query')
         # if not q:
@@ -78,17 +122,13 @@ class CamDictSpider(scrapy.Spider):
         # il.add_value('q', q)
         # il.add_value('url', m.group('base') + m.group('expr'))
         il.add_value('q', response.meta.get('q'))
-        il.add_value('url', response.request.url)
-        il.add_css('expr', '.dhw')
+        il.add_value('url', response.url)
+        il.add_css('expr', 'span.dhw::text')
         il.add_css('cat', '.dpos')
         il.add_css('pron', '.uk')
         il.add_css('define', '#cald4-1-1+ .dsense_b .ddef_d')
         print(il.load_item())
         return il.load_item()
-
-    # @staticmethod
-    # def inquiry(word):
-    #     yield scrapy.Request('https://dictionary.cambridge.org/dictionary/english/%s' % word)
 
 
 process = CrawlerProcess()
